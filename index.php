4ea281cc051eb5c4a597556723589474c5a235e0
Daniel <ingdsag@gmail.com>
2015-08-26 02:09:13 +0100

    Finally the query is sorted as I want now

--- index.php
+++ index.php
@@ -22,7 +22,10 @@
     fSData.cs = "&client_secret=1SVS4PP2THIDE2RLH0KOBFROE1NA140TMP2K4M05NWDBPRHK";
     fSData.llStr = "";
     fSData.query = "";
-    fSData.misc = "&v=20140806&m=foursquare&intent=checkin&radius=10000";
+    fSData.radius = "&radius=10000"
+    fSData.misc = "&v=20140806&m=foursquare&intent=checkin";
+    var markersArray = [];
+
     var map;
     function initMap()
     {
@@ -34,8 +37,11 @@
 
     function queryFourSquare()
     {
+        //Get query location without coordinates, after this, if successfull, we will get the coordinates, else we will get an error message
         var fSData = window.fSData;
-        var query = "https://api.foursquare.com/v2/venues/search?"+fSData.cid+fSData.cs+fSData.llStr+fSData.query+fSData.misc;
+        window.fSData.llStr = "&ll="+0+","+0;
+        var query = "https://api.foursquare.com/v2/venues/search?"+fSData.cid+fSData.cs+fSData.query+fSData.misc;
+        console.log(query);
         $.ajax({
             url: query,
             context: document.body
@@ -44,8 +50,43 @@
             var foundLocation = e.response.venues;
             if (typeof(foundLocation[0]) !="undefined")
             {
-                moveToLocation(foundLocation[0].location.lat, foundLocation[0].location.lng);
-                for(var i=0;i<foundLocation.length;i++)
+                //If successfull, we will get all venues with the new found coordinates
+                window.fSData.llStr = "&ll="+foundLocation[0].location.lat+","+foundLocation[0].location.lng;
+                window.fSData.lat = foundLocation[0].location.lat;
+                window.fSData.lng = foundLocation[0].location.lng;
+                //Centering the map in the new coordinates
+                moveToLocation();
+                //Getting all the locations and stablishing the markers
+                getAllVenuesCloseToLocation()
+            }
+            else
+            {
+                showMessage("Sorry, no locations found by that name")
+            }
+        });
+    }
+    function getAllVenuesCloseToLocation()
+    {
+        //Again, another ajax query, but this time without the query variable, so that it wont mess with our search, this time, with a radius limitation
+        var query = "https://api.foursquare.com/v2/venues/search?"+fSData.cid+fSData.cs+fSData.llStr+fSData.misc+fSData.radius;
+        $.ajax({
+            url: query,
+            context: document.body
+        }).done(function(e)
+        {
+            var foundLocation = e.response.venues;
+            //Clean all the markers, everytime the user makes a query
+            for(var i=0;i<markersArray.length;i++)
+            {
+                markersArray[i].setMap(null);
+            }
+            markersArray = null;
+            //Brand new healthy markers Array
+            markersArray = [];
+            //Add the new markers, if at least one location is found
+            if (typeof(foundLocation[0]) !="undefined")
+            {
+                for(i=0;i<foundLocation.length;i++)
                 {
                     var singleLocation = foundLocation[i];
                     var myLatLng = {lat: singleLocation.location.lat, lng: singleLocation.location.lng};
@@ -54,42 +95,32 @@
                         map: map,
                         title: singleLocation.name
                     });
-                    //setMapOnAll.setMap(null);
-                    //
-                    //location.lat
-                    //location.lng
-                    console.log(foundLocation[i]);
+                    //organize the markers
+                    markersArray.push(marker);
                 }
             }
         });
     }
-    function moveToLocation(lat, lng)
+    function moveToLocation()
     {
-        var center = new google.maps.LatLng(lat,lng);
+        var center = new google.maps.LatLng(window.fSData.lat,window.fSData.lng);
         map.panTo(center);
     }
     function getCoords(position)
     {
-        window.fSData.llStr = "&ll="+position.coords.latitude+","+position.coords.longitude;
-        window.fSData.lat = position.coords.latitude;
-        window.fSData.lon = position.coords.longitude;
         queryFourSquare();
     }
     function getQueryResults(e)
     {
+        //Click or submit query
         e.preventDefault();
-        window.fSData.query = "&query="+encodeURIComponent($("#query").val());
-        if (navigator.geolocation)
-        {
-            navigator.geolocation.getCurrentPosition(getCoords);
-        }
-        else
-        {
-
-        }
+        window.fSData.query = "&near="+encodeURIComponent($("#query").val());
+        queryFourSquare();
+    }
+    function showMessage(msg)
+    {
 
     }
-
 
 </script>
 
